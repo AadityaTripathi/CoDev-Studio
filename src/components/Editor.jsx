dad661a9019ed21edbcfdffd9af924eb2b8411ff
AadityaTripathi <tripathiaaditya02@gmail.com>
2024-04-15 00:33:37 +0530

    implemented the code editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Codemirror from 'codemirror';
+import "codemirror/lib/codemirror.css"
+import "codemirror/mode/javascript/javascript";
+import "codemirror/theme/dracula.css";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+
 
 const Editor = () => {
-    return <div>Editor goees here</div>
+    useEffect(() => {
+        async function init(){
+            Codemirror.fromTextArea(document.getElementById("realtimeeditor"),{
+                mode: { name: 'javascript', json: true},
+                theme: "dracula",
+                autoCloseTags: true,
+                autoCloseBrackets: true,
+                lineNumbers: true
+            });
+        }
+        init();
+    }, [])
+    return <textarea id="realtimeeditor"></textarea>
 }
 
 export default Editor;
