a6505597805013942312f152cc7338864b4593ca
AadityaTripathi <tripathiaaditya02@gmail.com>
2024-04-14 23:44:12 +0530

    added editor page and uuid

---
+++ src/components/Editor.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Editor = () => {
+    return <div>Editor goees here</div>
+}
+
+export default Editor;
--- src/pages/Editorpage.jsx
+++ src/pages/Editorpage.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 
 const Editorpage = () => {
-    return <div>Editor Page</div>
+    const [clients, setClients] = useState([
+        {socketId: 1, username: "Aaditya T"},
+        {socketId: 2, username: "Momo Mami"},
+        {socketId: 3, username: "Sexy Sucks"}
+    ])
+
+
+    return (
+        <div className="mainwrapper">
+            <div className="sidepanel">
+                <div className="sidepanel-inner">
+                    <div className="logo2">
+                        <img src="/codev.png" 
+                        className="logo2img" 
+                        alt="logo" 
+                        />
+                    </div>
+                    <h3>Connected:</h3>
+                    <div className="clientList">
+                        {clients.map((client) => (
+                            <Client 
+                                key = {client.socketId}
+                                username = {client.username}>
+                            </Client>
+                        ))}
+                    </div>
+                </div>
+                <button className="btn copyBtn">Copy Room ID</button>
+                <button className="btn leaveBtn">Leave Room</button>
+            </div>
+            <div className="editorwrapper">
+                <Editor></Editor>
+            </div>
+        </div>
+    );
 }
 
 export default Editorpage;
