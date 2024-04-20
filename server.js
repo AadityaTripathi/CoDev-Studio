c3bfe6284bf741148547f5864565a2081a13ec28
AadityaTripathi <tripathiaaditya02@gmail.com>
2024-04-20 20:46:40 +0530

    trying to deploy3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
+console.log("env value",process.env.PORT)
 
 const app = express();
 const server = http.createServer(app);
